ac577041e0b4bc47baefe640c25308516e368ecb
Arzamastsev Dmitry <motok1996@gmail.com>
2022-02-20 20:44:59 +0300

    v4

---
+++ report.py
@@ -0,0 +1,12 @@
+import pandas as pd
+students_avg_scores = {'Max': 4.964, 'Eric': 4.962, 'Peter': 4.923, 'Mark': 4.957, 'Julie': 4.95, 'Jimmy': 4.973,
+                       'Felix': 4.937, 'Vasya': 4.911, 'Don': 4.936, 'Zoi': 4.937}
+
+
+def make_report_about_top3(avg):
+    res = sorted(avg.items(), key=lambda x: x[1], reverse=True)
+    df = pd.DataFrame(res[:3])
+    df.to_csv('result.csv', index=False)
+
+
+make_report_about_top3(students_avg_scores)
