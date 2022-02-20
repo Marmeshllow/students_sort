f79ddc5c18ee0148ef7bb19d3a6c8bfd5a50011a
Arzamastsev Dmitry <motok1996@gmail.com>
2022-02-20 19:45:10 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+con = [
+    {'name': 'vasya', 'scores': {'math': 58, 'res_lang': 62, 'cs': 48}, 'extra': 0},    # 168
+    {'name': 'fedya', 'scores': {'math': 33, 'res_lang': 85, 'cs': 42}, 'extra': 2},    # 162
+    {'name': 'petya1', 'scores': {'math': 92, 'res_lang': 33, 'cs': 34}, 'extra': 1},   # 160
+    {'name': 'petya2', 'scores': {'math': 100, 'res_lang': 30, 'cs': 29}, 'extra': 1},  # 160
+]
+
+
+def top_20(con_lst):
+    res = sorted(con_lst, key=lambda x: (x['scores']['math'] + x['scores']['res_lang'] + x['scores']['cs'] + x['extra'],
+                 x['scores']['math'] + x['scores']['cs']),
+                 reverse=True)
+    return res[:20]
